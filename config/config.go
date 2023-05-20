76dc6885bff8e3f1117937ff3c533923f5cea506
mszsabab <mszsabab22@gmail.com>
2023-05-20 22:08:16 +0600

    configs added

--- config/config.go
+++ config/config.go
@@ -1,1 +1,14 @@
 package config
+
+import "github.com/caarlos0/env/v6"
+
+type Config struct {
+	ApplicationPort int    `env:"APPLICATION_PORT"`
+	DatabaseUrl     string `env:"DATABASE_URL"`
+}
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	err := env.Parse(cfg)
+	return cfg, err
+}
