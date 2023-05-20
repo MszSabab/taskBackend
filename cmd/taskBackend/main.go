62d406aedc71abb00dabcfb9e1a273562619799c
mszsabab <mszsabab22@gmail.com>
2023-05-20 22:07:58 +0600

    main file added

--- cmd/taskBackend/main.go
+++ cmd/taskBackend/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"github.com/taskBackend/config"
+	"github.com/taskBackend/httpserver"
+	"github.com/taskBackend/repository"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db, err := repository.NewPgRepository(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rr := httpserver.NewServer(
+		cfg,
+		db,
+	)
+	rr.Serve()
+}
