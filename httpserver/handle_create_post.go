ff67cb936e981fdfacf1014e75bef370e79e0a04
mszsabab <mszsabab22@gmail.com>
2023-05-20 21:57:39 +0600

    create post api added

---
+++ httpserver/handle_create_post.go
@@ -0,0 +1,51 @@
+package httpserver
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) handleCreatePost() http.HandlerFunc {
+
+	type Comment struct {
+		ID      int    `json:"id"`
+		Content string `json:"content"`
+		User    string `json:"user"`
+	}
+
+	type Post struct {
+		ID       int       `json:"id"`
+		Title    string    `json:"title"`
+		Content  string    `json:"content"`
+		MediaURL string    `json:"mediaURL"`
+		Likes    int       `json:"likes"`
+		Comments []Comment `json:"comments"`
+	}
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+		post := Post{}
+		err := json.NewDecoder(req.Body).Decode(&post)
+		if err != nil {
+			// TODO add log
+			return
+		}
+
+		s.db.Insert(post.Title, post.Content, post.MediaURL, post.ID)
+		//if err != nil {
+		//	log.Println(err)
+		//	w.WriteHeader(http.StatusInternalServerError)
+		//	return
+		//}
+
+		post.Likes = 0
+
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(post)
+		if err != nil {
+			// TODO add log
+			return
+		}
+	}
+}
