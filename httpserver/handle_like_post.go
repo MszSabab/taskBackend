f6d109b4dff20c26d7068ec49415e4848ef823d7
mszsabab <mszsabab22@gmail.com>
2023-05-20 22:10:00 +0600

    working on like post

--- httpserver/handle_like_post.go
+++ httpserver/handle_like_post.go
@@ -1,1 +1,23 @@
 package httpserver
+
+import (
+	"net/http"
+)
+
+func (s *Server) handleLikePost() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+		//postID, _ := strconv.Atoi(chi.URLParam(req, "id"))
+		//
+		//_, err := db.Exec("UPDATE posts SET likes = likes + 1 WHERE id = $1", postID)
+		//if err != nil {
+		//	log.Println(err)
+		//	w.WriteHeader(http.StatusInternalServerError)
+		//	return
+		//}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
