b6f85f44a2dbd294dc7243350c7706027f8b8fd0
mszsabab <mszsabab22@gmail.com>
2023-05-22 02:14:21 +0600

    query added

--- repository/postgres.go
+++ repository/postgres.go
@@ -4,7 +4,7 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/jmoiron/sqlx"
 	"github.com/taskBackend/config"
-	"log"
+	"time"
 )
 
 type PgRepository struct {
@@ -21,12 +21,81 @@ func NewPgRepository(cfg *config.Config) (*PgRepository, error) {
 	}, nil
 }
 
-func (pr *PgRepository) Create(Title, Content, MediaURL string, ID int) {
+func (pr *PgRepository) CreatePost(likes int, Title, Content, MediaURL string) error {
+	_, err := pr.db.Exec(
+		`INSERT INTO post (likes, title, content, media_url) VALUES ($1, $2, $3, $4)`,
+		likes,
+		Title,
+		Content,
+		MediaURL,
+	)
+	return err
+}
 
-	err := pr.db.QueryRow("INSERT INTO posts (title, content, media_url) VALUES ($1, $2, $3) RETURNING id", Title, Content, MediaURL).Scan(&ID)
-	if err != nil {
-		log.Println(err)
-		//w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+type Comment struct {
+	ID       int    `json:"id"`
+	Content  string `json:"content"`
+	UserName string `db:"user_name"`
+}
+
+type getAllPosts struct {
+	Id        string    `db:"id"`
+	Title     string    `db:"title"`
+	Content   string    `db:"content"`
+	MediaURL  string    `db:"media_url"`
+	Likes     int       `db:"likes"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func (pr *PgRepository) GetAllPosts(limit, offset int) (*[]getAllPosts, error) {
+	getAll := make([]getAllPosts, 0)
+	err := pr.db.Select(
+		&getAll,
+		`
+		SELECT *
+		FROM post
+		LIMIT $1 OFFSET $2
+		`,
+		limit,
+		offset,
+	)
+	return &getAll, err
+}
+
+func (pr *PgRepository) LikePost(id int) error {
+	_, err := pr.db.Exec(
+		`
+		UPDATE post
+    	SET likes = likes + 1
+		WHERE id = $1
+		`,
+		id,
+	)
+	return err
+}
+
+func (pr *PgRepository) UnLikePost(id int) error {
+	_, err := pr.db.Exec(
+		`
+		UPDATE post
+    	SET likes = likes - 1
+		WHERE id = $1
+		`,
+		id,
+	)
+	return err
+}
+
+func (pr *PgRepository) AddComment(content, username string, postID int) error {
+	_, err := pr.db.Exec(
+		`
+		INSERT INTO comments
+		(content, user_name, post_id) VALUES ($1, $2, $3)
+		`,
+		content,
+		username,
+		postID,
+	)
+	return err
 }
